59d36a5823b22c14d93311ce8bc88d837883d46e
Ryan Stadther <47157604+ryanstadther@users.noreply.github.com>
2025-02-11 11:33:37 -0600

    Update streamlit_app.py
    added smoothiefroot api integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -19,6 +20,10 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections=5
 )
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 if ingredients_list:
     ingredients_string = ''
